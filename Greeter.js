6109c21afcc87780e370ab488118f17178bcb3d4
Danny Vega <dannyevega@gmail.com>
2017-10-28 15:14:03 -0700

    added functionality for jquery

--- Greeter.js
+++ Greeter.js
@@ -11,18 +11,117 @@
 		return new Greeter.init(firstName, lastName, language);		
 	}
 	
+	// languages, greetings, formalGreetings and printMessages are not exposed outside of this context until we tell it do so
+	var languages = ["english", "spanish"];
+	
+	var greetings = {
+		english: "Hello",		
+		spanish: "Hola"
+	};
+	
+	var formalGreetings = {
+		english: "Greetings",
+		spanish: "Saludos"
+	};
+	
+	var printMessages = {
+		english: "Logged in",
+		spanish: "Indicío sesión"
+	};
+	
 	// Initially set prototype to an empty Object - well store any methods we want to use on our Object thats returned from calling Greeter
-	Greeter.prototype = {};
+	Greeter.prototype = {
+		
+		fullName: function(){
+			return this.firstName + " " + this.lastName;
+		},
+		
+		validate: function(){
+			if(languages.indexOf(this.language) === -1){
+				throw new Error("Invalid language");
+			}
+		},
+		
+		greeting: function(){
+			return greetings[this.language] + " " + this.firstName + "!";
+		},
+		
+		formalGreeting: function(){
+			return formalGreetings[this.language] + ", " + this.fullName();
+		},
+		
+		// greet function is what well be using mostly - just pass what type of greeting you want
+		greet: function(formal){
+			var message;
+			
+			if(formal){
+				message = this.formalGreeting();
+			} else  {
+				message = this.greeting();
+			}
+			
+			if(console){
+				console.log(message);
+			}
+			
+			// 'this' refers to calling the Object at execution time - makes the method chainable like jQuery
+			return this;
+		},
+		
+		log: function(){
+			// because IE is stupid and doesnt have a console variable unless its opened
+			if(console){
+				console.log(printMessages[this.language] + ": " + this.fullName());
+			}
+			
+			// make it chainable
+			return this;
+		},
+		
+		setLanguage: function(language){
+			// set language
+			this.language = language;
+			
+			// make sure its valid
+			this.validate();
+			
+			// make it chainable
+			return this;
+		},
+		
+		DOMGreeting: function(selector, formal){
+			if(!$){
+				throw new Error("jQuery is not loaded");
+			}
+			
+			if(!selector){
+				throw new Error("Missing jQuery selector");
+			}
+			
+			var message;
+			if(formal){
+				message = this.formalGreeting();
+			} else  {
+				message = this.greeting();
+			}
+			
+			$(selector).html(message);
+			
+			return this;
+		}
+		
+	};
 	
 	// here's the function constructor that creates Objects
 	Greeter.init = function(firstName, lastName, language){		
 		var self = this;		
 		self.firstName = firstName || "";
 		self.lastName = lastName || "";
-		self.language = language || "English";
+		self.language = language || "english";
+		self.validate();
 	}
 	
-	// Any Object created from Greeter.init function constructor will point here - set it equal to Greeter.prototype so all Objects will inherin any methods we include there
+	// Any Object created from Greeter.init function constructor will point here - set it equal to Greeter.prototype so all Objects will inherit any methods we include there
 	// i.e. any Objects created from this function, should point to Greeter.prototype for its prototype chain
 	Greeter.init.prototype = Greeter.prototype;
 	
--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var danny = G$("Danny", "Vega");
-console.log(danny);
+//danny.greet().setLanguage('spanish').greet(true).log();
+
+// when we click the button...
+$('#login').click(function(){
+	
+	// this creates a new Object using our Greeter function
+	var loginGreeter = G$("Danny", "Vega");
+	
+	// hide the UI using jQuery
+	$("#login-container").hide();
+	
+	// set the language based on whats selected in the select box - chain the DOMGreeting method to udpate the DOM and then log it to the console
+	loginGreeter.setLanguage($("#language").val()).DOMGreeting('#greeting', true).log();
+	
+})
